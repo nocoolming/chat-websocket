36571d007a798eb0cd8490353e949137f926a5d4
finch <21374618@qq.com>
2019-05-07 17:32:16 +0800

    跑通

--- src/main/java/org/ming/chat/api/ChatController.java
+++ src/main/java/org/ming/chat/api/ChatController.java
@@ -7,7 +7,6 @@ import org.ming.chat.store.SessionStore;
 import org.ming.chat.util.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
